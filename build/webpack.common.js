4bbb09e40955ac01d66f7f801920f2651df08cb3
yanlibo2021 <yanlibo2021@126.com>
2020-04-21 14:48:59 +0800

    new file:   .babelrc.js         modified:   .flowconfig         modified:   build/webpack.common.js         modified:   package-lock.json         modified:   package.json

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -2,11 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-
+function resolve(dir) {
+    return path.join(__dirname, "..", dir);
+}
 module.exports = {
     context: path.resolve(__dirname, "../"),
     entry: {
-        app: "./src/index.js"
+        // app: "./src/index.js"
+        app: "./lib/vuejs/platforms/web/entry-runtime-with-compiler.js"
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -21,9 +24,23 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
         ]
-    }
+    },
+    resolve: {
+        // extensions: [".js", ".vue", ".json"],
+        alias: {
+            "compiler": resolve("lib/vuejs/compiler"),
+            "core": resolve("lib/vuejs/core"),
+            "shared": resolve("lib/vuejs/shared"),
+            "web": resolve("lib/vuejs/platforms/web"),
+            "weex": resolve("lib/vuejs/platforms/weex")
+        }
+    },
 };
